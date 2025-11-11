fa3337215491667fcc8fb3dbed9517f2c9a61c15
inn-goose <nadelyaev.ilya@gmail.com>
2025-11-11 22:11:22 +0000

    cleanup wiring

--- eeprom_programmer/eeprom_programmer_wiring.h
+++ eeprom_programmer/eeprom_programmer_wiring.h
@@ -26,6 +26,7 @@ ChipType str_to_chip_type(const String& chip_type) {
 
 typedef uint8_t PIN_NO;
 
+
 // ========================================
 // DIP28 WIRING
 // ========================================
@@ -78,6 +79,7 @@ const PIN_NO DIP28_WIRING[28] = {
   0,   // 28 / VCC
 };
 
+
 // ========================================
 // Chip WIRING
 // ========================================
@@ -105,9 +107,9 @@ static const PIN_NO ADDRESS_BUS_PINS[ADDRESS_BUS_SIZE] = { 10, 9, 8, 7, 6, 5, 4,
 static const size_t DATA_BUS_SIZE = 8;
 static const PIN_NO DATA_BUS_PINS[DATA_BUS_SIZE] = { 11, 12, 13, 15, 16, 17, 18, 19 };
 static const size_t MANAGEMENT_SIZE = 4;
-static const PIN_NO MANAGEMENT_PINS[MANAGEMENT_SIZE] = { 20, 22, 27, 1 };  // !CE, !OE, !WE, [!BSY]
-// static const PIN_NO NC_PINS[1] = { 26 };
+static const PIN_NO MANAGEMENT_PINS[MANAGEMENT_SIZE] = { 20, 22, 27, 1 };  // !CE, !OE, !WE, !BSY
 }
+
 
 // AT28C256 / DIP28
 
@@ -127,10 +129,12 @@ static const PIN_NO MANAGEMENT_PINS[MANAGEMENT_SIZE] = { 20, 22, 27, 1 };  // !C
 // 14 -- | GND   IO3 |-- 15
 
 class AT28C256 {
-  // static const uint8_t ADDRESS_PINS[] = { 10, 9, 8, 7, 6, 5, 4, 3, 25, 24, 21, 23, 2, 26, 1 };
-  // static const uint8_t DATA_PINS[] = { 11, 12, 13, 15, 16, 17, 18, 19 };
-  // static const uint8_t MANAGEMENT_PINS[4] = { 20, 22, 27, 0 };  // !CE, !OE, !WE, [!BSY]
-  // static const uint8_t NC_PINS[] = {};
+  static const size_t ADDRESS_BUS_SIZE = 15;
+  static const PIN_NO ADDRESS_BUS_PINS[ADDRESS_BUS_SIZE] = { 10, 9, 8, 7, 6, 5, 4, 3, 25, 24, 21, 23, 2, 26, 1 };
+  static const size_t DATA_BUS_SIZE = 8;
+  static const PIN_NO DATA_BUS_PINS[DATA_BUS_SIZE] = { 11, 12, 13, 15, 16, 17, 18, 19 };
+  static const size_t MANAGEMENT_SIZE = 4;
+  static const PIN_NO MANAGEMENT_PINS[MANAGEMENT_SIZE] = { 20, 22, 27, 0 };  // !CE, !OE, !WE, [!BSY]
 };
 
 
@@ -299,60 +303,6 @@ private:
   WiringType _wiring_type;
   ChipType _chip_type;
 };
-
-// ========================================
-// !!! REDEFINE FOR YOUR WIRING SCHEME
-// ========================================
-
-// PIN -- !BSY -- |    | -- VCC  -- VCC
-// PIN --  A12 -- |    | -- !WE  -- PIN
-// PIN --   A7 -- |    | -- NC   -- –––
-// PIN --   A6 -- |    | -- A8   -- PIN
-// PIN --   A5 -- |    | -- A9   -- PIN
-// PIN --   A4 -- |    | -- A11  -- PIN
-// PIN --   A3 -- |    | -- !OE  -- PIN
-// PIN --   A2 -- |    | -- A10  -- PIN
-// PIN --   A1 -- |    | -- !CE  -- PIN
-// PIN --   A0 -- |    | -- IO7  -- PIN
-// PIN --  IO0 -- |    | -- IO6  -- PIN
-// PIN --  IO1 -- |    | -- IO5  -- PIN
-// PIN --  IO2 -- |    | -- IO4  -- PIN
-// GND --  GND -- |    | -- IO3  -- PIN
-
-// wire pins from 0 to N
-// const uint8_t EEPROM_ADDRESS_PINS[] = { A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12 };
-// const uint8_t EEPROM_DATA_PINS[] = { IO0, IO1, IO2, IO3, IO4, IO5, IO6, IO7 };
-// const uint8_t EEPROM_CHIP_ENABLE_PIN = CE;
-// const uint8_t EEPROM_OUTPUT_ENABLE_PIN = OE;
-// const uint8_t EEPROM_WRITE_ENABLE_PIN = WE;
-// const uint8_t EEPROM_READY_BUSY_OUTPUT_PIN = BSY;
-
-// ========================================
-// MEGA
-// ========================================
-
-// 29 -- !BSY -- |    | -- VCC  -- VCC
-// 31 --  A12 -- |    | -- !WE  -- 22
-// 33 --   A7 -- |    | -- NC   -- 24
-// 35 --   A6 -- |    | -- A8   -- 26
-// 37 --   A5 -- |    | -- A9   -- 28
-// 39 --   A4 -- |    | -- A11  -- 30
-// 41 --   A3 -- |    | -- !OE  -- 32
-// 43 --   A2 -- |    | -- A10  -- 34
-// 45 --   A1 -- |    | -- !CE  -- 36
-// 47 --   A0 -- |    | -- IO7  -- 38
-// 49 --  IO0 -- |    | -- IO6  -- 40
-// 51 --  IO1 -- |    | -- IO5  -- 42
-// 53 --  IO2 -- |    | -- IO4  -- 44
-// GND -- GND -- |    | -- IO3  -- 46
-
-const uint8_t EEPROM_ADDRESS_PINS[] = { 47, 45, 43, 41, 39, 37, 35, 33, 26, 28, 34, 30, 31 };
-const uint8_t EEPROM_DATA_PINS[] = { 49, 51, 53, 46, 44, 42, 40, 38 };
-const uint8_t EEPROM_CHIP_ENABLE_PIN = 36;
-const uint8_t EEPROM_OUTPUT_ENABLE_PIN = 32;
-const uint8_t EEPROM_WRITE_ENABLE_PIN = 22;
-const uint8_t EEPROM_READY_BUSY_OUTPUT_PIN = 29;
-const uint8_t NON_CONNECTED_PINS[] = { 24 };
 
 }  // EepromProgrammerWiring
 
